44a1ab992adac169a451657c5557ed18f5445b2b
faseela <faseelavs.faz@gmail.com>
2022-01-20 21:59:46 +0530

    fullname length chngd

--- server/validation/auth.js
+++ server/validation/auth.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const ValidateSignup = (userData) => {
   const Schema = joi.object({
-    fullName: joi.string().required().min(5),
+    fullName: joi.string().required().min(3),
     email: joi.string().email().required(),
     password: joi.string(),
     address: joi
