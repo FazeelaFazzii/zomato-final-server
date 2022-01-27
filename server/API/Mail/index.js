095eb0ded09e3f25d27fe65596ffb6d6fbc2be1d
faseela <faseelavs.faz@gmail.com>
2022-01-28 01:45:30 +0530

    from to

--- server/API/Mail/index.js
+++ server/API/Mail/index.js
@@ -6,9 +6,15 @@ const Router = express.Router();
 
 Router.post("/", async (req, res) => {
     try{
-        const { from, to, subject, text } = req.body.mailData;
+       // const { from, to, subject, text } = req.body.mailData;
         const sendMail = initializeMailgun();
-        await sendMail.messages().send({ from, to, subject,text });
+        const data = {
+            
+            to:"faseelavs.faz@gmail.com",
+            subject:"hello",
+            text:"mailgun testing"
+        };
+        await sendMail.messages().send(data);
         return res.status(200).json({ status: "Email has been sent" });
     }catch(error){
         return res.status(500).json({ error: error.message });
